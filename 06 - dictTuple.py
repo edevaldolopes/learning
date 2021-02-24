e202d4377c0a98ff12075472fffd185eb768ad12
Edevaldo <edevaldosla@gmail.com>
2021-02-24 19:12:17 -0300

    Merge remote-tracking branch 'origin/main' into main

---
+++ 06 - dictTuple.py
@@ -0,0 +1,30 @@
+l2 = list()
+t2 = tuple()
+d2 = dict()
+s2 = set()
+
+l1 = ['anna', 'luna', 'sende']
+t1 = ('anna', 'luna', 'sende')
+d1 = {'kid1': 'anna', 'kid2': 'luna', 'mother': 'sende'}
+s1 = {'anna', 'luna', 'sende'}
+
+# list
+l1.append('edevaldo')
+l1.remove('edevaldo')
+l1.count('anna')
+l1.pop()
+len(l1)
+
+# tuple
+t2.count('luna')
+len(t2)
+
+# dict
+d1['dog'] = 'slink'
+d1['father'] = 'edevaldo'
+del d1['father']
+
+# set
+s1.add('edevaldo')
+s1.remove('edevaldo')
+len(s1)
