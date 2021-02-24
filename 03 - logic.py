d8f320144f93797ed2794fb3b86852507807fafb
DevLopez <78511646+edevaldolopes@users.noreply.github.com>
2021-02-24 11:01:09 -0300

    Add files via upload

---
+++ 03 - logic.py
@@ -0,0 +1,17 @@
+var_true = True
+print(type(var_true))
+
+# comparison
+number = 10
+print(number > 0)
+print(number < 0)
+print(number >= 0)
+print(number <= 0)
+print(number == 0)
+print(number != 0)
+print(number is False)
+print(number is True)
+print(number is not True)
+print(number is not False)
+
+# logic
