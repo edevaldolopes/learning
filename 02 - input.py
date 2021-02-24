fff461781ceeae313013d777c0fa2ecb5f792605
DevLopez <78511646+edevaldolopes@users.noreply.github.com>
2021-02-24 11:01:09 -0300

    Add files via upload

---
+++ 02 - input.py
@@ -0,0 +1,34 @@
+# data input
+data = input('Enter your name: ')
+
+# show message
+print('Hello World')
+print('Test hello World in Pycharm')
+
+# variable
+name = 'Bill'
+lastName = 'Gates'
+age = 55
+weight = 68.5
+male = True
+
+# tips \n
+print(name)
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(male))
+
+# concatenate
+print('My name is:', name, lastName)
+print('I have ' + str(age) + ' age!!!')  # cast
+print('My name is: {} {}'.format(name, lastName))
+
+# math
+print(2 + 2)
+print(3 - 2)
+print(2 * 2)
+print(2 ** 3)
+print(9 / 2)
+print(9 // 2)
+print(9 % 2)
---
+++ 03 - logic.py
@@ -0,0 +1,22 @@
+var_true = True
+number = 10
+print(type(var_true))
+
+# comparison
+number = 10
+print(number > 0)
+print(number < 0)
+print(number >= 0)
+print(number <= 0)
+print(number == 0)
+print(number != 0)
+print(number is False)
+print(number is True)
+print(number is not True)
+print(number is not False)
+
+# logic
+if number > 0:
+    print('Your is best!!!')
+else:
+    print('No is best!!!')
