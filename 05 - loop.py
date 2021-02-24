3d388a0a9ac6a88c6f69875fdd48db8cb45673f5
Edevaldo <edevaldosla@gmail.com>
2021-02-24 17:06:18 -0300

    Merge remote-tracking branch 'origin/main' into main

--- 05 - loop.py
+++ 05 - loop.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
+# list
 l1 = ['gol', 'fox', 'uno', 's10', 'd20']
 
 # for in
 for car in l1:
     print(car)
 
-for car in range(0, 11, 2):
-    print()
+n1 = 0
+for x in range(1, 5):
+    x += n1
+print(x)
+
+# while
+s = 0
+x = 1
+while x < 100:
+    s += x
+    x += 1
+print(s)
